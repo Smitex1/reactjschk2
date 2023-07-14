6daba7e8389d884f7739925fde877aaec4739dd1
smitex1 <smithpraise61@gmail.com>
2023-07-14 11:28:14 +0100

    my first commit

---
+++ react-js-checkpointe-master/src/component/Profile/Address.js
@@ -0,0 +1,17 @@
+
+
+const Address = () => {
+
+  const address = "No 53 alagomeji yaba 10001"
+
+  return (
+    <div>
+      <address style={{color:"white"}}>
+        {address}
+      </address>
+
+    </div>
+  )
+}
+
+export default Address
